29d1c08dda94e0c6944b2a4ad05ac30ca2dc5c4b
Will Zhang <wz282@cornell.edu>
2024-01-12 16:06:58 -0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by wizha on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
